3a03db6c689e7b2aaa8a156dbd0392648fd8c136
Benjamin Clos <benjamin.clos@gmail.com>
2020-12-17 18:16:17 -0700

    feat: add millisecond formatting support;

--- lib/format.rb
+++ lib/format.rb
@@ -1,5 +1,6 @@
 module Format
   def self.time(seconds)
+    return milliseconds(seconds) if seconds < 1
     days    = (seconds / 86_400).floor
     seconds = seconds - (days * 86_400)
     hours   = (seconds / 3_600).floor
@@ -7,11 +8,16 @@ module Format
     minutes = (seconds / 60).floor
     seconds = (seconds - (minutes * 60)).floor
 
+
     [days, hours, minutes, seconds]
       .zip(%w(d h m s))
       .select { |f| f.first > 0 }
       .map {|f| f.first.to_s.rjust(2, "0") + f.last }
       .reduce("") { |acc, col| acc + " " + col }
       .strip
+  end
+
+  def self.milliseconds(seconds)
+    "%s ms" % ((seconds * 1_000).round).to_i
   end
 end
